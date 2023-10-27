0d0fa929b66d76100911b2b6340b210439c7e00a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-27 18:36:43 +0530

    added logo in aspect ratio

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,26 @@
 import Link from "next/link";
+import { AspectRatio } from "./ui/aspect-ratio";
+import Image from "next/image";
+import LogoImage from "@logos/black.svg";
 
 const Logo = () => {
-	return <Link href="/">Go Home</Link>;
+	return (
+		<Link href="/" prefetch={false} className="overflow-hidden">
+			<div className="flex items-center w-72 h-14">
+				<AspectRatio
+					ratio={16 / 9}
+					className="flex items-center justify-center"
+				>
+					<Image
+						priority
+						src={LogoImage}
+						alt="logo"
+						className="dark:filter dark:invert"
+					/>
+				</AspectRatio>
+			</div>
+		</Link>
+	);
 };
 
 export default Logo;
