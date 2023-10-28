fdea81ae5355516f2c4aeb347099c95ef1afb89e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-28 17:32:17 +0530

    build the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/ui/Header";
 import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import ClientProvider from "@/components/ui/ClientProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Translation - SaaS",
@@ -20,7 +17,7 @@ export default function RootLayout({
 	return (
 		<ClientProvider>
 			<html lang="en">
-				<body className={inter.className}>
+				<body className="flex flex-col min-h-screen">
 					<ThemeProvider
 						attribute="class"
 						defaultTheme="system"
