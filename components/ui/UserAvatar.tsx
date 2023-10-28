a53a242528c86f6251067aea9465e61b9df54ab3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-28 09:59:38 +0530

    added avatar fallback

--- components/ui/UserAvatar.tsx
+++ components/ui/UserAvatar.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Image from "next/image";
 
 const UserAvatar = ({
@@ -22,7 +22,15 @@ const UserAvatar = ({
 					className="rounded-full"
 				/>
 			)}
-			<AvatarFallback>CN</AvatarFallback>
+			<AvatarFallback
+				delayMs={1000}
+				className="dark:bg-white dark:text-black text-lg"
+			>
+				{name
+					.split(" ")
+					.map((n) => n[0])
+					.join("")}
+			</AvatarFallback>
 		</Avatar>
 	);
 };
