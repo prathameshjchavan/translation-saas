a9cdb62bfe30307b8b344b4cabc30988bef4b032
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-30 11:05:58 +0530

    added SubscriptionProvider

--- components/SubscriptionProvider.tsx
+++ components/SubscriptionProvider.tsx
@@ -1,14 +1,36 @@
 "use client";
 
+import { subscriptionRef } from "@/lib/converters/Subscription";
+import { useSubscriptionStore } from "@/store/store";
+import { onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
 
-const SubscriptionProvider = () => {
+const SubscriptionProvider = ({ children }: { children: React.ReactNode }) => {
 	const { data: session } = useSession();
+	const setSubscription = useSubscriptionStore(
+		(state) => state.setSubscription
+	);
 
-	useEffect(() => {}, []);
+	useEffect(() => {
+		if (!session) return;
 
-	return <div>SubscriptionProvider</div>;
+		return onSnapshot(
+			subscriptionRef(session?.user.id),
+			(snapshot) => {
+				if (snapshot.empty) {
+					console.log("User has NO subscription");
+					setSubscription(null);
+					return;
+				} else {
+					setSubscription(snapshot.docs[0].data());
+				}
+			},
+			(error) => console.log("Error getting document:", error)
+		);
+	}, [session, setSubscription]);
+
+	return <Fragment>{children}</Fragment>;
 };
 
 export default SubscriptionProvider;
