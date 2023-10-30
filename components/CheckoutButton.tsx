f067377874783afd1e09d47effef78fcbb4def3c
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-10-30 13:40:54 +0530

    added loading spinner in checkout button

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -4,6 +4,7 @@ import { db } from "@/firebase";
 import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 const CheckoutButton = () => {
 	const { data: session } = useSession();
@@ -53,7 +54,7 @@ const CheckoutButton = () => {
 				onClick={() => createCheckoutSession()}
 				className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default"
 			>
-				{loading ? "loading..." : "Sign Up"}
+				{loading ? <LoadingSpinner /> : "Sign Up"}
 			</button>
 		</div>
 	);
