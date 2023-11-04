c50f306a89fd97b21c6f0fb746ac47aea821111f
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-04 18:33:11 +0530

    added dropdown options

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -12,9 +12,13 @@ import UserAvatar from "./UserAvatar";
 import { Session } from "next-auth";
 import { Button } from "./ui/button";
 import { signIn, signOut } from "next-auth/react";
+import { useSubscriptionStore } from "@/store/store";
+import LoadingSpinner from "./LoadingSpinner";
+import { Fragment } from "react";
+import { StarIcon } from "lucide-react";
 
 const UserButton = ({ session }: { session: Session | null }) => {
-	// Subscription Listenter
+	const subscription = useSubscriptionStore((state) => state.subscription);
 
 	if (!session)
 		return (
@@ -28,9 +32,30 @@ const UserButton = ({ session }: { session: Session | null }) => {
 			<DropdownMenuTrigger>
 				<UserAvatar name={session.user?.name} image={session.user?.image} />
 			</DropdownMenuTrigger>
-			<DropdownMenuContent>
+			<DropdownMenuContent
+				className="text-center
+"
+			>
 				<DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
 				<DropdownMenuSeparator />
+				{subscription === undefined && (
+					<DropdownMenuItem>
+						<LoadingSpinner />
+					</DropdownMenuItem>
+				)}
+				{subscription?.role === "pro" && (
+					<Fragment>
+						<DropdownMenuLabel className="text-xs flex items-center justify-center space-x-1 text-[#E935C1] animate-pulse">
+							<StarIcon fill="#E935C1" />
+							<p>PRO</p>
+						</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							Manage
+							{/* <ManageAccountButton /> */}
+						</DropdownMenuItem>
+					</Fragment>
+				)}
 				<DropdownMenuItem onClick={() => signOut()}>Sign Out</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
