015779ca4a013a8f2b69464f6bd2150617badf0e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-05 13:41:48 +0530

    implemented chats

--- components/ChatListRows.tsx
+++ components/ChatListRows.tsx
@@ -4,8 +4,11 @@ import {
 	ChatMembers,
 	chatMembersCollectionGroupRef,
 } from "@/lib/converters/ChatMembers";
+import { MessageSquare } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { useCollectionData } from "react-firebase-hooks/firestore";
+import CreateChatButton from "./CreateChatButton";
+import ChatListRow from "./ChatListRow";
 
 const ChatListRows = ({ initialChats }: { initialChats: ChatMembers[] }) => {
 	const { data: session } = useSession();
@@ -16,7 +19,25 @@ const ChatListRows = ({ initialChats }: { initialChats: ChatMembers[] }) => {
 		}
 	);
 
-	return <div>ChatListRows</div>;
+	if (!members?.length)
+		return (
+			<div className="flex flex-col justify-center items-center pt-40 space-y-2">
+				<MessageSquare className="h-10 w-10" />
+				<h1 className="text-5xl font-extralight">Welcome!</h1>
+				<h2 className="pb-10">
+					Lets get you started by creating your first chat!
+				</h2>
+				<CreateChatButton isLarge />
+			</div>
+		);
+
+	return (
+		<div className="">
+			{members.map((member, index) => (
+				<ChatListRow key={member.chatId} chatId={member.chatId} />
+			))}
+		</div>
+	);
 };
 
 export default ChatListRows;
