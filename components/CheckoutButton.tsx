1ef8027057c9b9481efe40c47d254a8967611c76
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-06 13:48:48 +0530

    added subcribed message

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -3,7 +3,7 @@
 import { db } from "@/firebase";
 import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import LoadingSpinner from "./LoadingSpinner";
 import { useSubscriptionStore } from "@/store/store";
 import ManageAccountButton from "./ManageAccountButton";
@@ -57,6 +57,15 @@ const CheckoutButton = () => {
 	return (
 		<div className="flex flex-col space-y-2">
 			{/* If subscribed show me the user is subscribed */}
+			{isSubscribed && (
+				<Fragment>
+					<hr className="mt-5" />
+					<p className="pt-5 text-center text-xs text-indigo-600">
+						You are subscribed to PRO
+					</p>
+				</Fragment>
+			)}
+
 			<div className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
 				{isSubscribed ? (
 					<ManageAccountButton />
