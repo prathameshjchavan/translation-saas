999f4da40bcb0e5d2a4a80d3e6410f312a58eaee
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-11-09 07:57:23 +0530

    bug fixes

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -2,7 +2,7 @@ import PricingCards from "@/components/PricingCards";
 
 const PricingPage = () => {
 	return (
-		<div className="isolate overflow-hidden bg-gray-900">
+		<div className="isolate overflow-y-scroll bg-gray-900">
 			<div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
 				<div className="mx-auto max-w-4xl">
 					<h2 className="text-base font-semibold leading-7 text-indigo-400">
